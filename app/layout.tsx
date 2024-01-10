5dbe7717b886aabcd6dd65671e2b6ff3c2259dcc
wirapratamaa <wirapratama758@gmail.com>
2024-01-10 13:00:45 +0800

    update props type

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,16 @@ import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
+type LayoutPropsType = {
+  children: React.ReactNode;
+};
+
 export const metadata = {
   title: "Bali Ayu Shop",
   description: "Aromatherapy",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: LayoutPropsType) {
   return (
     <html lang="en">
       <body className={inter.className}>
