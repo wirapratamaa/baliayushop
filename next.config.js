0eeb9ac2922bbe9615c508bb5463aa55d319e888
wirapratamaa <wirapratama758@gmail.com>
2024-01-10 13:09:08 +0800

    update next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "images.pexels.com" }],
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
   },
 };
 
