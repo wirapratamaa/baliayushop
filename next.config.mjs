97daea74142fd92a4b35cf106343728d368d94b2
wirapratamaa <wirapratama758@gmail.com>
2026-02-09 12:11:46 +0800

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
-
-import withPlaiceholder from "@plaiceholder/next";
-
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [{ protocol: "https", hostname: "images.pexels.com" }],
-    reactStrictMode: true,
   },
 };
 
-export default withPlaiceholder(nextConfig);
+export default nextConfig;
